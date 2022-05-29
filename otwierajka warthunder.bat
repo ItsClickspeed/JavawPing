007f4b11edf5d82ae6b3dbba19437bf49b3834f0
ItsClickspeed <48134914+ItsClickspeed@users.noreply.github.com>
2022-05-29 10:53:20 -0400

    Delete otwierajka warthunder.bat

--- otwierajka warthunder.bat
+++
@@ -1,8 +0,0 @@
-﻿@echo off
-echo "wlacz wt i kliknij na ta probowke na dole, wtedy ci sie pojawi pojazd z napisami sample"
-set /p tak="Zrestartuj WT jak to zrobisz przejdz dalej.Aby przejsc dalej napisz cokolwiek"
-mkdir UserSkins
-start .\UserSkins
-echo "w folderze ktory sie otworzyl powinien byc folder o nazwie twojego czolgu"
-set /p tak="Aby przejsc dalej napisz cokolwiek"
-echo "teraz mozesz go edytowac!"
